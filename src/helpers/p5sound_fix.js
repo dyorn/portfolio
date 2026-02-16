11d7ebb45bec4899ae191067f34fc95349f52040
maxemitchell <maxemitchell@gmail.com>
2026-02-15 18:57:05 -0800

    code art bugfix, better blog post image sizing

--- src/helpers/p5sound_fix.js
+++ src/helpers/p5sound_fix.js
@@ -1,2 +1,6 @@
 import * as p5 from 'p5'
 window.p5 = p5
+
+// Disable AudioWorklet to prevent RingBuffer race condition in p5.sound 1.3.1
+// Forces fallback to ScriptProcessorNode which doesn't have this bug
+window.AudioWorkletNode = undefined
